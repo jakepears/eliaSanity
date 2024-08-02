8fa750cbb40d3bcf6d412a13411d842a706aa693
jakepears <jake.pearson.1694@gmail.com>
2024-08-01 18:39:45 -0700

    hi

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/** @format */
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	build: {
+		rollupOptions: {
+			external: ['@sanity/vision'],
+		},
+	},
+});
