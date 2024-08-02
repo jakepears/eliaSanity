ff1ccff6b5a96037333affc23b8df6a0fe31caa1
jakepears <jake.pearson.1694@gmail.com>
2024-08-01 18:48:16 -0700

    config changes

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	build: {
 		rollupOptions: {
-			external: ['@sanity/vision'],
+			external: [
+				'@sanity/vision',
+				'sanity-plugin-asset-source-unsplash',
+				'sanity-plugin-mux-input',
+				'sanity/structure',
+			],
 		},
 	},
 });
